eb5e4983bbab0e476287bd027f9c15bfc27d3475
Indrian Ningsih <Indrianningsih01@gmail.com>
2025-06-24 16:07:45 +0800

    Sisip Bunga di posisi tertentu

--- TokoBunga.cpp
+++ TokoBunga.cpp
@@ -22,3 +22,23 @@ void addBunga() {
     inventaris.push_back(b);
     cout << "Bunga berhasil ditambahkan!\n";
 }
+
+void insertBunga() {
+    int pos;
+    Bunga b;
+    cout << "\nMasukkan posisi untuk menyisipkan: ";
+    cin >> pos;
+    if (pos < 0 || pos > inventaris.size()) {
+        cout << "Posisi tidak valid!\n";
+        return;
+    }
+    cout << "Masukkan nama bunga: ";
+    cin.ignore();
+    getline(cin, b.nama);
+    cout << "Masukkan stok: ";
+    cin >> b.stok;
+    cout << "Masukkan harga: ";
+    cin >> b.harga;
+    inventaris.insert(inventaris.begin() + pos, b);
+    cout << "Bunga berhasil disisipkan!\n";
+}
